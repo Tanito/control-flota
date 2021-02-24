2ffede9f2e1712baed04847a9d5659edc8c14e10
Tanito <ulises.d83@gmail.com>
2021-02-24 19:53:14 -0300

    new icons

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "yourAkiKey",
-    authDomain: "yourAuthDomain",
-    databaseURL: "databaseURL",
-    projectId: "projectId",
-    storageBucket: "storageBucket",
-    messagingSenderId: "messagingSenderId",
-    appId: "appId"
+    apiKey: "AIzaSyBjc87KeuG_RQhckjlb8MNNeZRvj9q8474",
+    authDomain: "flota-baeda.firebaseapp.com",
+    databaseURL: "https://flota-baeda-default-rtdb.firebaseio.com",
+    projectId: "flota-baeda",
+    storageBucket: "flota-baeda.appspot.com",
+    messagingSenderId: "670869047187",
+    appId: "1:670869047187:web:d3c9ea8a426c941442ca66"
   }
 };
