0373552c50a3b9e664438f2fd5345b7d79b8e7a6
Tanito <ulises.d83@gmail.com>
2021-02-25 20:51:27 -0300

    environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBjc87KeuG_RQhckjlb8MNNeZRvj9q8474",
-    authDomain: "flota-baeda.firebaseapp.com",
-    databaseURL: "https://flota-baeda-default-rtdb.firebaseio.com",
-    projectId: "flota-baeda",
-    storageBucket: "flota-baeda.appspot.com",
-    messagingSenderId: "670869047187",
-    appId: "1:670869047187:web:d3c9ea8a426c941442ca66"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
